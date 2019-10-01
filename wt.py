40796622abc8da2284b09b128382c8bce7239c44
suzhi82 <suzhi82@gmail.com>
2019-10-01 17:47:21 +0930

    added signal handler

--- wt.py
+++ wt.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys, xlrd 
+import sys, xlrd, signal
+
+def signal_handler(signal, frame):
+  print("\nYou pressed Ctrl+C!")
+  sys.exit(0)
+
+signal.signal(signal.SIGINT,signal_handler)
 
 def read_excel():
   # check the arguments
